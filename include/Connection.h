e795c2c13403b396c2061af26eb791158a283ec3
Michael Wycombe <michael.wycombe@gmail.com>
2025-07-12 20:16:24 -0700

    Update Connection.h

---
+++ include/Connection.h
@@ -0,0 +1,17 @@
+#ifndef CONNECTION_H_DEF
+#define CONNECTION_H_DEF
+#include <cstdint>
+
+namespace connection
+
+{
+  struct Connection {
+      int32_t targetNeuronSlot {};            // where to enqueue the signal
+      int32_t lastSignalOriginTime {};        // last time a signal was issued on this connection - used for STP/LTP
+      int32_t temporalDistanceToTarget{};     // time to deliver to target
+      int16_t stpWeight{};                    // current weight for stp amplification
+      int16_t ltpWeigh{};                     // current weight for ltp amplification
+  };
+
+}   // end of connection namespace
+#endif
